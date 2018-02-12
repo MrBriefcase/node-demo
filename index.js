bf249c6eb34d9efbbc5274576a7e28059919abae
Daren Capacio <megamanv5@hotmail.com>
2018-02-11 21:45:12 -0800

    testing

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ var greetings=[
     "Priviet"
 ]
 
+// ALLOW ACCESS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/greeting", function(req, resp){
     var index=Math.random()*(greetings.length-1);
     index=Math.round(index);
