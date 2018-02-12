7a37e9b7be224033c165db9a98247019c892052e
Daren Capacio <megamanv5@hotmail.com>
2018-02-11 22:58:38 -0800

    test

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var greetings=[
 
 // ALLOW ACCESS *************
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://socketpractice.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
