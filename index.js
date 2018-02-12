155738a0414d09a793dfe2e1495c977f3ae92598
Daren Capacio <megamanv5@hotmail.com>
2018-02-11 21:30:06 -0800

    testing

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
-const express=require("express");
-var app=express();
-const port=15000;
+// server
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 5000);
 
-var greetings=[
-    "hola",
-    "hey dudes",
-    "Nihaoma",
-    "Salam Refighi",
-    "Ahlan Va sahlan",
-    "Priviet"
-]
-
-app.get("/greeting", function(req, resp){
-    var index=Math.random()*(greetings.length-1);
-    index=Math.round(index);
-    resp.end(greetings[index]);
-});
-//this where we receive the greeting and push it into our array
-app.get("/add/:greet", (req, resp)=>{
-    var greet=req.params.greet;
-    greetings.push(greet);
-    resp.end("a new greeting added");
-});
-// BASE
-app.get("/", (req, resp)=>{
-    res.send('Hello Node + GitHub!');
+// Our first route
+app.get('/', function (req, res) {
+  res.send('Hello Node + GitHub!');
 });
 
-app.listen(port, function(err){
-    if(err){
-        console.log("hey something is wrong");
-        return false;
-    }
-    else{
-        console.log("all good go ahead the gate is open");
-    }
+// Listen to port
+app.listen(app.get('port'), function () {
+  console.log('App is listening on port ' + app.get('port'));
 });
-
 
 
 
