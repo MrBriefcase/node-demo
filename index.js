35e211d5901de5a4fea294b1691adf7819514ffc
Daren Capacio <megamanv5@hotmail.com>
2018-02-12 17:50:18 -0800

    testing

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var chatBox = 'Welcome to the chat app!\n\n';
 
 // ALLOW ACCESS *************
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://youradaren.herokuapp.com");
     res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
